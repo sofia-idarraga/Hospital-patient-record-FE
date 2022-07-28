90de6b9281780ef6d8abdb163caf9e36ea3dcd29
Sofia Idarraga <sofia.idarraga@udea.edu.co>
2022-07-28 13:35:38 -0500

    buttons created

--- src/js/elements.js
+++ src/js/elements.js
@@ -20,6 +20,23 @@ function display(speciality) {
     divTitle.append(h2, h3);
     div.append(divTitle);
     speciality.patients.forEach(patient => displayPatient(patient, div));
+    const divButtons = document.createElement('div');
+    divButtons.id = "buttons";
+    divButtons.className = "w-full mt-10 p-6";
+    const editButton = document.createElement('button');
+    const createPatientButton = document.createElement('button');
+    editButton.className = "w-full block bg-gray-900 font-medium text-xl p-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-black";
+    createPatientButton.className = "w-full block bg-gray-900 font-medium text-xl p-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-black";
+    editButton.innerText = "Edit";
+    createPatientButton.innerText = "New Patient";
+    if (speciality.patients.length == 0) {
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "Delete";
+        deleteButton.className = "w-full block bg-gray-900 font-medium text-xl p-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-black";
+        divButtons.append(deleteButton);
+    }
+    divButtons.append(editButton, createPatientButton);
+    div.append(divButtons);
     specialityContainer.append(div);
 }
 function displayPatient(patient, divSpeciality) {
@@ -43,6 +60,20 @@ function displayPatient(patient, divSpeciality) {
     ul.append(liAge, liDni, liDates);
     divPatientData.append(ul);
     div.append(patientName, divPatientData);
+    const divButtons = document.createElement('div');
+    divButtons.className = "flex place-items-center text-center mx-auto";
+    const deleteButton = document.createElement('button');
+    const editButton = document.createElement('button');
+    editButton.className = " block p-2.5 bg-gray-900 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white";
+    editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+  </svg>`;
+    deleteButton.className = " block p-2.5 bg-gray-900 rounded-xl hover:rounded-3xl hover:bg-red-700 transition-all duration-300 text-white";
+    deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+  </svg>`;
+    divButtons.append(editButton, deleteButton);
+    div.append(divButtons);
     divSpeciality.append(div);
 }
 export { specialityContainer, displaySpecialities };
--- src/ts/elements.ts
+++ src/ts/elements.ts
@@ -27,7 +27,28 @@ function display(speciality:specialityI){
     divTitle.append(h2,h3);
     div.append(divTitle);
     speciality.patients.forEach(patient => displayPatient(patient, div));
-    specialityContainer.append(div);
+
+    const divButtons:HTMLDivElement = document.createElement('div');
+    divButtons.id = "buttons";
+    divButtons.className = "w-full mt-10 p-6";
+    const editButton:HTMLButtonElement = document.createElement('button');
+    const createPatientButton:HTMLButtonElement = document.createElement('button');
+    editButton.className = "w-full block bg-gray-900 font-medium text-xl p-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-black";
+    createPatientButton.className = "w-full block bg-gray-900 font-medium text-xl p-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-black";
+
+    editButton.innerText = "Edit";
+    createPatientButton.innerText = "New Patient";
+    if(speciality.patients.length == 0){
+        const deleteButton:HTMLButtonElement = document.createElement('button');
+        deleteButton.innerText="Delete";
+        deleteButton.className = "w-full block bg-gray-900 font-medium text-xl p-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-black";
+        divButtons.append(deleteButton);
+    }
+    divButtons.append(editButton, createPatientButton);
+
+    div.append(divButtons);
+
+    specialityContainer.append(div);  
 
 } 
 
@@ -55,8 +76,24 @@ function displayPatient(patient:Patient, divSpeciality: HTMLDivElement){
 
     ul.append(liAge,liDni,liDates);
     divPatientData.append(ul);
-
     div.append(patientName, divPatientData);
+
+    const divButtons:HTMLDivElement = document.createElement('div');
+    divButtons.className = "flex place-items-center text-center mx-auto";
+    const deleteButton:HTMLButtonElement = document.createElement('button');
+    const editButton:HTMLButtonElement = document.createElement('button');
+    
+    editButton.className = " block p-2.5 bg-gray-900 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white";
+    editButton.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+  </svg>`;
+
+    deleteButton.className = " block p-2.5 bg-gray-900 rounded-xl hover:rounded-3xl hover:bg-red-700 transition-all duration-300 text-white";
+    deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+  </svg>`;
+    divButtons.append(editButton, deleteButton);
+    div.append(divButtons);
     divSpeciality.append(div);
 }
 
